19954b1831c7e7ab1e4d45d6571ebb061b979c23
Shusaku Uesugi <shu@chibicode.com>
2020-07-31 00:45:00 -0700

    Mix existing JS

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <div>Hello World!</div>
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `
+    <div class="foo">Hello World</div>
+    <script>
+      document.querySelector('foo').style = 'color: red;'
+    </script>
+  `
+      }}
+    />
+  )
 }
